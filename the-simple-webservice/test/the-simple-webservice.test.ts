f0756c3250cdfe4dd19d9c07532da93f600b5b26
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2020-01-19 10:30:17 +0000

    tests added

--- the-simple-webservice/test/the-simple-webservice.test.ts
+++ the-simple-webservice/test/the-simple-webservice.test.ts
@@ -1,13 +1,69 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, haveResourceLike, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheSimpleWebservice = require('../lib/the-simple-webservice-stack');
 
-test('Empty Stack', () => {
+test('DynamoDB Created', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new TheSimpleWebservice.TheSimpleWebserviceStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  expectCDK(stack).to(haveResourceLike("AWS::DynamoDB::Table", {
+    "KeySchema": [
+      {
+        "AttributeName": "path",
+        "KeyType": "HASH"
+      }
+    ]}
+  ));
+});
+
+test('DynamoDB Read/Write IAM Policy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheSimpleWebservice.TheSimpleWebserviceStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+    "PolicyDocument": {
+      "Statement": [
+        {
+        "Action": [
+          "dynamodb:BatchGetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ],
+        "Effect": "Allow"  
+      }]
+    }
+  }
+  ));
+});
+
+test('DynamoDB Lambda Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheSimpleWebservice.TheSimpleWebserviceStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+    "Handler": "lambda.handler",
+    "Runtime": "nodejs12.x"
+  }
+  ));
+});
+
+test('API Gateway Proxy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheSimpleWebservice.TheSimpleWebserviceStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Resource", {
+    "PathPart": "{proxy+}"
+  }
+  ));
 });
