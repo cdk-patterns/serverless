f09d6326f1519cf5a31e972d26548dae6002dad6
z8674558 <own7000hr@gmail.com>
2020-01-19 09:56:40 +0000

    updating lambda runtime version

--- the-simple-webservice/lib/the-simple-webservice-stack.ts
+++ the-simple-webservice/lib/the-simple-webservice-stack.ts
@@ -14,7 +14,7 @@ export class TheSimpleWebserviceStack extends cdk.Stack {
 
      // defines an AWS Lambda resource
      const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_8_10,      // execution environment
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
       code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
       handler: 'lambda.handler',                // file is "lambda", function is "handler"
       environment: {
