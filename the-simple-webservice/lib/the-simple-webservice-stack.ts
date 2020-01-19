e4560a892dbe30fad3136ef3ce4824b3a3b87d00
z8674558 <own7000hr@gmail.com>
2020-01-19 09:45:13 +0000

    adding in constructs

--- the-simple-webservice/lib/the-simple-webservice-stack.ts
+++ the-simple-webservice/lib/the-simple-webservice-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
 
 export class TheSimpleWebserviceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //DynamoDB Table
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+     // defines an AWS Lambda resource
+     const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_8_10,      // execution environment
+      code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
+      handler: 'lambda.handler',                // file is "lambda", function is "handler"
+      environment: {
+        HITS_TABLE_NAME: table.tableName
+    }
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
