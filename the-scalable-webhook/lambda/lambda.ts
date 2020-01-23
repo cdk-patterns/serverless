b278a77efde05cfd03f5e95cfd83c396acb3873e
z8674558 <own7000hr@gmail.com>
2020-01-23 20:43:11 +0000

    adding in lambda and left hand side of arch pre sqs

---
+++ the-scalable-webhook/lambda/lambda.ts
@@ -0,0 +1,19 @@
+const { DynamoDB, Lambda } = require('aws-sdk');
+
+exports.handler = async function(event:any) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+
+  // return response back to upstream caller
+  return sendRes(200, 'You have connected with the Lambda!');
+};
+
+const sendRes = (status:number, body:string) => {
+  var response = {
+    statusCode: status,
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: body
+  };
+  return response;
+};
