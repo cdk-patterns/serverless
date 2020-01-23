cc0f0e9f31749d247dd362b7e51d584686d03969
NIDeveloper <ec2-user@ip-172-31-90-88.ec2.internal>
2020-01-23 20:43:11 +0000

    adding in lambda and left hand side of arch pre sqs

--- the-scalable-webhook/lib/the-scalable-webhook-stack.ts
+++ the-scalable-webhook/lib/the-scalable-webhook-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class TheScalableWebhookStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+     // defines an AWS Lambda resource
+     const sqsPublishLambda = new lambda.Function(this, 'SQSPublishLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
+      code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
+      handler: 'lambda.handler',                // file is "lambda", function is "handler"
+      environment: {
+    }
+    });
+
+    // defines an API Gateway REST API resource backed by our "dynamoLambda" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: sqsPublishLambda
+    });
   }
 }
