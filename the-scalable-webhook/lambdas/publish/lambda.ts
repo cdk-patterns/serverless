18199c9745435b4c7b5c8665147781c914afe6ae
z8674558 <own7000hr@gmail.com>
2020-01-24 10:09:18 +0000

    adding in logic to publish to queue

--- the-scalable-webhook/lambdas/publish/lambda.ts
+++ the-scalable-webhook/lambdas/publish/lambda.ts
@@ -1,10 +1,43 @@
-const { DynamoDB, Lambda } = require('aws-sdk');
+var AWS = require('aws-sdk');
 
 exports.handler = async function(event:any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
+  // Create an SQS service object
+  var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+  var params = {
+    DelaySeconds: 10,
+    MessageAttributes: {
+      "Title": {
+        DataType: "String",
+        StringValue: "The Whistler"
+      },
+      "Author": {
+        DataType: "String",
+        StringValue: "John Grisham"
+      },
+      "WeeksOn": {
+        DataType: "Number",
+        StringValue: "6"
+      }
+    },
+    MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+    // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+    // MessageId: "Group1",  // Required for FIFO queues
+    QueueUrl: process.env.queueURL
+  };
+
+  await sqs.sendMessage(params, function(err:any, data:any) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.MessageId);
+    }
+  }).promise();
+
   // return response back to upstream caller
-  return sendRes(200, 'You have connected with the Lambda!');
+  return sendRes(200, 'You have added a message to the queue!');
 };
 
 let sendRes = (status:number, body:string) => {
--- the-scalable-webhook/lib/the-scalable-webhook-stack.ts
+++ the-scalable-webhook/lib/the-scalable-webhook-stack.ts
@@ -12,51 +12,22 @@ export class TheScalableWebhookStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     // defines an AWS Lambda resource to publish to our queue
-     const sqsPublishLambda = new lambda.Function(this, 'SQSPublishLambdaHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
-      code: lambda.Code.asset('lambdas/publish'),  // code loaded from the "lambda" directory
-      handler: 'lambda.handler',                // file is "lambda", function is "handler"
-      environment: {
-    }
-    });
-    
-    // defines an AWS Lambda resource to pull from our queue
-     const sqsSubscribeLambda = new lambda.Function(this, 'SQSSubscribeLambdaHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
-      code: lambda.Code.asset('lambdas/subscribe'),  // code loaded from the "lambda" directory
-      handler: 'lambda.handler',                // file is "lambda", function is "handler"
-      reservedConcurrentExecutions: 2, // throttle lambda to 2 concurrent invocations
-      environment: {
-    }
-    });
-
-    // defines an API Gateway REST API resource backed by our "sqsPublishLambda" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: sqsPublishLambda
-    });
-    
-    //SNS/SQS
-    const queue = new sqs.Queue(this, 'RDSPublishQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'RDSPublishTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-    
+    // Basic VPC Setup (RDS has to be in a VPC)
     const vpc = new Vpc(this, 'TheVPC', {
-       cidr: "10.0.0.0/16"
+      cidr: "10.0.0.0/16"
     })
-    
-    //RDS
-    let secret = new Secret(this, 'secret', {
-        description: 'rds password',
-        secretName: 'rds-password',
-        generateSecretString: {
-            excludePunctuation: true,
-            excludeCharacters: '/@" '
-        }
+
+    /**
+     * RDS Database Setup
+     * MySQL Database / Auto Generated Password Setup
+     */
+    /*let secret = new Secret(this, 'secret', {
+      description: 'rds password',
+      secretName: 'rds-password',
+      generateSecretString: {
+          excludePunctuation: true,
+          excludeCharacters: '/@" '
+      }
     })
     
     let mySQLRDSInstance = new DatabaseInstance(this, 'mysql-rds-instance', {
@@ -75,6 +46,58 @@ export class TheScalableWebhookStack extends cdk.Stack {
         databaseName: 'webhook',
         masterUserPassword: secret.secretValue,
         port: 3306
+    });*/
+
+    /**
+     * Queue Setup
+     * SNS/SQS creation
+     */
+    const queue = new sqs.Queue(this, 'RDSPublishQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
     });
+
+    /*const topic = new sns.Topic(this, 'RDSPublishTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));*/
+
+    /**
+     * Lambdas
+     * Both publisher and subscriber from pattern
+     */
+
+    // defines an AWS Lambda resource to publish to our queue
+    const sqsPublishLambda = new lambda.Function(this, 'SQSPublishLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
+      code: lambda.Code.asset('lambdas/publish'),  // code loaded from the "lambdas/publish" directory
+      handler: 'lambda.handler',                // file is "lambda", function is "handler"
+      environment: {
+        queueURL: queue.queueUrl
+      }
+    });
+    
+    queue.grantSendMessages(sqsPublishLambda);
+
+    // defines an AWS Lambda resource to pull from our queue
+    const sqsSubscribeLambda = new lambda.Function(this, 'SQSSubscribeLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
+      code: lambda.Code.asset('lambdas/subscribe'),  // code loaded from the "lambdas/subscribe" directory
+      handler: 'lambda.handler',                // file is "lambda", function is "handler"
+      reservedConcurrentExecutions: 2, // throttle lambda to 2 concurrent invocations
+      environment: {
+      }
+    });
+
+    queue.grantConsumeMessages(sqsSubscribeLambda);
+
+    /**
+     * API Gateway Proxy
+     * Used to expose the webhook through a URL
+     */
+
+    // defines an API Gateway REST API resource backed by our "sqsPublishLambda" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: sqsPublishLambda
+    });
+    
   }
 }
