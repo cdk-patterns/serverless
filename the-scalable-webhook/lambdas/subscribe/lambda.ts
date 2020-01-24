15115cfb27b4b1991af4fbed8a60e4ea4d8cd804
z8674558 <own7000hr@gmail.com>
2020-01-24 11:02:57 +0000

    subscriber now works

--- the-scalable-webhook/lambdas/subscribe/lambda.ts
+++ the-scalable-webhook/lambdas/subscribe/lambda.ts
@@ -3,37 +3,10 @@ var AWS = require('aws-sdk');
 exports.handler = async function(event:any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
-  // Create an SQS service object
-  var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-
-  var params = {
-    AttributeNames: [
-       "SentTimestamp"
-    ],
-    MaxNumberOfMessages: 10,
-    MessageAttributeNames: [
-       "All"
-    ],
-    QueueUrl: process.env.queueURL,
-    VisibilityTimeout: 20,
-    WaitTimeSeconds: 0
-  };
-
-  sqs.receiveMessage(params, function(err:any, data:any) {
-    if (err) {
-      console.log("Error pulling message from queue", err);
-    } else if (data.Messages) {
-      var deleteParams = {
-        QueueUrl: process.env.queueURL,
-        ReceiptHandle: data.Messages[0].ReceiptHandle
-      };
-      sqs.deleteMessage(deleteParams, function(err:any, data:any) {
-        if (err) {
-          console.log("Delete Error", err);
-        } else {
-          console.log("Message Deleted From Queue Successfully", data);
-        }
-      });
-    }
-  });
+  let records: any[] = event.Records;
+  
+  for(let index in records) {
+    let payload = records[index].body;
+    console.log('received message ' + payload);
+  }
 };
