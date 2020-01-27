d42c685d13a47979cd178c4f380d7bf25c78fe38
z8674558 <own7000hr@gmail.com>
2020-01-27 22:31:51 +0000

    pattern now streams to lamba correctlt

--- the-dynamo-streamer/lambdas/subscribe/lambda.ts
+++ the-dynamo-streamer/lambdas/subscribe/lambda.ts
@@ -1,13 +1,3 @@
-const { DynamoDB } = require('aws-sdk');
-
 exports.handler = async function(event:any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
-
-  let records: any[] = event.Records;
-  
-  for(let index in records) {
-    let payload = records[index].body;
-    let id = records[index].messageAttributes.MessageDeduplicationId.stringValue
-    console.log('received message ' + payload);
-  }
 };
