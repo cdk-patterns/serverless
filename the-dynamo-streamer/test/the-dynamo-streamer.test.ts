c4d1325e6110cb9c1bd7b45ce8d307cb02e594dd
z8674558 <own7000hr@gmail.com>
2020-01-28 19:28:33 +0000

    dynamo tests added

--- the-dynamo-streamer/test/the-dynamo-streamer.test.ts
+++ the-dynamo-streamer/test/the-dynamo-streamer.test.ts
@@ -1,13 +1,47 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheDynamoStreamer = require('../lib/the-dynamo-streamer-stack');
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('DynamoDB Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::DynamoDB::Table", {
+    "KeySchema": [
+      {
+        "AttributeName": "message",
+        "KeyType": "HASH"
+      }
+    ],
+    "StreamSpecification": {
+      "StreamViewType": "NEW_IMAGE"
+    }}
+  ));
+});
+
+test('DynamoDB Stream Read IAM Policy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+    "PolicyDocument": {
+      "Statement": [
+        {
+          "Action": "dynamodb:ListStreams",
+          "Effect": "Allow"
+        },
+        {
+          "Action": [
+            "dynamodb:DescribeStream",
+            "dynamodb:GetRecords",
+            "dynamodb:GetShardIterator"
+          ],
+          "Effect": "Allow"
+        }
+      ]
+    }
+  }
+  ));
 });
