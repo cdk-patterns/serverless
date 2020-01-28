2358865914a2a5d6565c18f02c2a42c7c9a502b4
z8674558 <own7000hr@gmail.com>
2020-01-28 19:41:38 +0000

    adding in remainder of tests

--- the-dynamo-streamer/test/the-dynamo-streamer.test.ts
+++ the-dynamo-streamer/test/the-dynamo-streamer.test.ts
@@ -45,3 +45,124 @@ test('DynamoDB Stream Read IAM Policy Created', () => {
   }
   ));
 });
+
+test('Lambda Event Source Mapping Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::EventSourceMapping", {
+    "BatchSize": 100,
+    "StartingPosition": "LATEST"
+  }));
+});
+
+
+test('API Gateway /InsertItem Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Resource", {
+    "PathPart": "InsertItem",
+  }
+  ));
+});
+
+test('API Gateway Method + VTL Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Method", {
+    "HttpMethod": "POST",
+    "AuthorizationType": "NONE",
+    "Integration": {
+      "IntegrationHttpMethod": "POST",
+          "IntegrationResponses": [
+            {
+              "ResponseTemplates": {
+                "application/json": "{\"message\":\"item added to db\"}"
+              },
+              "StatusCode": "200"
+            }
+          ],
+          "RequestTemplates": {
+            "application/json": {
+              "Fn::Join": [
+                "",
+                [
+                  "{\"TableName\":\"",
+                  {
+                    "Ref": "TheDynamoStreamer641C5E5B"
+                  },
+                  "\",\"Item\":{\"message\":{\"S\":\"$input.path('$.message')\"}}}"
+                ]
+              ]
+            }
+          },
+          "Type": "AWS",
+    },
+    "MethodResponses": [
+      {
+        "ResponseParameters": {
+          "method.response.header.Content-Type": true,
+          "method.response.header.Access-Control-Allow-Origin": true,
+          "method.response.header.Access-Control-Allow-Credentials": true
+        },
+        "StatusCode": "200"
+      }
+    ]
+  }
+  ));
+});
+
+test('API Gateway Model Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Model", {
+    "ContentType": "application/json",
+    "Name": "ResponseModel",
+    "Schema": {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "pollResponse",
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    }
+  }
+  ));
+});
+
+test('API Gateway -> DynamoDB Read/Write IAM Policy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDynamoStreamer.TheDynamoStreamerStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+    "PolicyDocument": {
+      "Statement": [
+        {
+        "Action": [
+          "dynamodb:BatchGetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ],
+        "Effect": "Allow"  
+      }]
+    }
+  }
+  ));
+});
