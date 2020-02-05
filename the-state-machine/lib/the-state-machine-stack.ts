9b12bc0a3900d8016b5659d9c1b47769e58c2ce2
NIDeveloper <ec2-user@ip-172-31-90-88.ec2.internal>
2020-02-05 21:05:49 +0000

    tests added

--- the-state-machine/lib/the-state-machine-stack.ts
+++ the-state-machine/lib/the-state-machine-stack.ts
@@ -14,7 +14,7 @@ export class TheStateMachineStack extends cdk.Stack {
      */
      
     //The first thing we need to do is see if they are asking for pineapple on a pizza
-    const pineappleCheckLambda = new lambda.Function(this, 'orderPizzaLambdaHandler', {
+    let pineappleCheckLambda = new lambda.Function(this, 'pineappleCheckLambdaHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
       code: lambda.Code.asset('lambdas'),     // code loaded from the "lambda" directory
       handler: 'orderPizza.handler'           // file is "orderPizza", function is "handler"
