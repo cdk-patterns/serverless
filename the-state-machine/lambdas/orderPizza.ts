e88850c9d9035c055b5a04e091c02c584bae8647
Matt C <matthew.coulter@liberty-it.co.uk>
2020-02-05 16:38:03 +0000

    some more config

--- the-state-machine/lambdas/orderPizza.ts
+++ the-state-machine/lambdas/orderPizza.ts
@@ -1,3 +1,5 @@
 exports.handler = async function(event:any) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+
+    return {'type': 'pepperoni'}
 }
--- the-state-machine/lambdas/stateMachineLambda.ts
+++ the-state-machine/lambdas/stateMachineLambda.ts
@@ -1,3 +1,16 @@
+let aws = require('aws-sdk')
 exports.handler = async function(event:any) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+    var params = {
+    stateMachineArn: process.env.statemachine_arn,
+    input: JSON.stringify({})
+    }
+    var stepfunctions = new aws.StepFunctions()
+    stepfunctions.startExecution(params, function (err:any, data:any) {
+    if (err) {
+        console.log('err while executing step function')
+    } else {
+        console.log('started execution of step function')
+    }
+    })
 }
