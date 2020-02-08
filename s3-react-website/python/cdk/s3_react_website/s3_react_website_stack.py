e2683a2bf87f6b289bb0d263462ec9df049b8b70
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2020-02-08 16:21:48 +0000

    react example python version added

---
+++ s3-react-website/python/cdk/s3_react_website/s3_react_website_stack.py
@@ -0,0 +1,10 @@
+from aws_cdk import core
+from spa_deploy import SPADeploy
+
+class S3ReactWebsiteStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        SPADeploy(self, 'S3ReactDeploy').create_basic_site(index_doc='index.html',
+                                                             website_folder='../website/dist/website')
