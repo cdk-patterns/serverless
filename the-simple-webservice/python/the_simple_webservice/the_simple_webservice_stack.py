72aeab01ce543beb488494f56cac4c5d63b50a14
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2020-02-08 13:17:54 +0000

    formatting and comments

--- the-simple-webservice/python/the_simple_webservice/the_simple_webservice_stack.py
+++ the-simple-webservice/python/the_simple_webservice/the_simple_webservice_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     aws_lambda as _lambda,
-    aws_apigateway as apigw,
-    aws_dynamodb as dynamodb,
+    aws_apigateway as api_gw,
+    aws_dynamodb as dynamo_db,
     core
 )
 
@@ -11,24 +11,24 @@ class TheSimpleWebserviceStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # DynamoDB Table
-        table = dynamodb.Table(self, "Hits",
-                               partition_key=dynamodb.Attribute(name="path", type=dynamodb.AttributeType.STRING)
-                               )
+        table = dynamo_db.Table(self, "Hits",
+                                partition_key=dynamo_db.Attribute(name="path", type=dynamo_db.AttributeType.STRING)
+                                )
 
         # defines an AWS  Lambda resource
-        dynamoLambda = _lambda.Function(self, "DynamoLambdaHandler",
-                                        runtime=_lambda.Runtime.NODEJS_12_X,
-                                        handler="lambda.handler",
-                                        code=_lambda.Code.from_asset("lambda"),
-                                        environment={
-                                            'HITS_TABLE_NAME': table.table_name
-                                        }
+        dynamo_lambda = _lambda.Function(self, "DynamoLambdaHandler",
+                                            runtime=_lambda.Runtime.NODEJS_12_X,
+                                            handler="lambda.handler",
+                                            code=_lambda.Code.from_asset("lambda"),
+                                            environment={
+                                                'HITS_TABLE_NAME': table.table_name
+                                            }
                                         )
 
         # grant the lambda role read/write permissions to our table'
-        table.grant_read_write_data(dynamoLambda)
+        table.grant_read_write_data(dynamo_lambda)
 
-        # defines an API Gateway REST API resource backed by our "dynamoLambda" function.
-        apigw.LambdaRestApi(self, 'Endpoint',
-                            handler=dynamoLambda
-                            )
+        # defines an API Gateway REST API resource backed by our "dynamo_lambda" function.
+        api_gw.LambdaRestApi(self, 'Endpoint',
+                             handler=dynamo_lambda
+                             )
