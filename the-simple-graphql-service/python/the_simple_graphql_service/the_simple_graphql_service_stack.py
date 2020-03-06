e9adf79ba82d048775645d453918fa21af28247a
z8674558 <own7000hr@gmail.com>
2020-03-06 11:14:20 +0000

    Python refactor

--- the-simple-graphql-service/python/the_simple_graphql_service/the_simple_graphql_service_stack.py
+++ the-simple-graphql-service/python/the_simple_graphql_service/the_simple_graphql_service_stack.py
@@ -6,99 +6,97 @@ from aws_cdk import (
     core
 )
 
+
 class TheSimpleGraphqlServiceStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Create a new Cognito User Pool
-        userPool = cognito.UserPool(self, 'UserPool',
-            sign_in_aliases=cognito.SignInAliases(username=True)
-        )
+        user_pool = cognito.UserPool(self, 'UserPool',
+                                     sign_in_aliases=cognito.SignInAliases(username=True)
+                                     )
 
         # Create a new AppSync GraphQL API
         api = appsync.GraphQLApi(self, 'Api',
-            name= "demoapi",
-            log_config={
-                appsync.LogConfig(field_log_level=appsync.FieldLogLevel.ALL)
-            },
-            #  User pool authorizer configuration
-            #  userPoolConfig={
-            #    userPool,
-            #    defaultAction: UserPoolDefaultAction.ALLOW,
-            #  },
-            schema_definition_file=join('__dirname', '/../', 'schema/schema.graphql'),
-        )
+                                 name="demoapi",
+                                 log_config=appsync.LogConfig(field_log_level=appsync.FieldLogLevel.ALL),
+                                 #  User pool authorizer configuration
+                                 #  userPoolConfig={
+                                 #    userPool,
+                                 #    defaultAction: UserPoolDefaultAction.ALLOW,
+                                 #  },
+                                 schema_definition_file="schema/schema.graphql"
+                                 )
 
         # Create new DynamoDB Table for Customer
-        customerTable = dynamo_db.Table(self, "CustomerTable",
-                                partition_key=dynamo_db.Attribute(name="id", type=dynamo_db.AttributeType.STRING)
-                                )
-                                
+        customer_table = dynamo_db.Table(self, "CustomerTable",
+                                         partition_key=dynamo_db.Attribute(name="id",
+                                                                           type=dynamo_db.AttributeType.STRING)
+                                         )
+
         # Add Customer DynamoDB as a Datasource for the Graphql API.
-        customerDS = api.add_dynamo_db_data_source('Customer', 'The customer data source', customerTable)
+        customer_ds = api.add_dynamo_db_data_source('Customer', 'The customer data source', customer_table)
 
         # Query Resolver to get all Customers
-        customerDS.createResolver(
-            type_name = 'Query',
-            field_ame = 'getCustomers',
-            request_mapping_template = appsync.MappingTemplate.dynamoDbScanTable(),
-            responseMappingTemplate = appsync.MappingTemplate.dynamoDbResultList(),
+        customer_ds.create_resolver(
+            type_name='Query',
+            field_name='getCustomers',
+            request_mapping_template=appsync.MappingTemplate.dynamo_db_scan_table(),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_list(),
         )
 
         # Query Resolver to get an individual Customer by their id
-        customerDS.createResolver(
-        type_name = 'Query',
-        field_ame = 'getCustomer',
-        request_mapping_template = appsync.MappingTemplate.dynamoDbGetItem('id', 'id'),
-        responseMappingTemplate = appsync.MappingTemplate.dynamo_db_result_item(),
+        customer_ds.create_resolver(
+            type_name='Query',
+            field_name='getCustomer',
+            request_mapping_template=appsync.MappingTemplate.dynamo_db_get_item('id', 'id'),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_item(),
         )
 
         # Mutation Resolver for adding a new Customer
-        customerDS.createResolver(
-        type_name = 'Mutation',
-        field_ame = 'addCustomer',
-        request_mapping_template = appsync.MappingTemplate.dynamo_db_put_item(
-            key=appsync.PrimaryKey.partition('id').auto(),
-            values=appsync.AttributeValues.projecting('customer')),
-        responseMappingTemplate = appsync.MappingTemplate.dynamo_db_result_item(),
+        customer_ds.create_resolver(
+            type_name='Mutation',
+            field_name='addCustomer',
+            request_mapping_template=appsync.MappingTemplate.dynamo_db_put_item(
+                key=appsync.PrimaryKey.partition('id').auto(),
+                values=appsync.Values.projecting('customer')
+            ),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_item()
         )
 
-        # Mutation Resolver for updating an exisiting Customer
-        customerDS.createResolver(
-        type_name = 'Mutation',
-        field_ame = 'saveCustomerWithFirstOrder',
-        request_mapping_template = appsync.MappingTemplate.dynamo_db_put_item(
-            key=appsync.PrimaryKey
-                .partition('order').auto()
-                .sort('customer').is_('customer.id'),
-            Values=
-                appsync.projecting('order')
-                .attribute('referral').is_('referral')),
-        responseMappingTemplate = appsync.MappingTemplate.dynamo_db_result_item(),
+        # Mutation Resolver for updating an existing Customer
+        customer_ds.create_resolver(
+            type_name='Mutation',
+            field_name='saveCustomerWithFirstOrder',
+            request_mapping_template=appsync.MappingTemplate.dynamo_db_put_item(
+                key=appsync.PrimaryKey.partition('order').auto().sort('customer').is_('customer.id'),
+                values=appsync.Values.projecting('order').attribute('referral').is_('referral')
+            ),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_item()
         )
 
-        # Mutation Resolver for deleting an exisiting Customer
-        customerDS.createResolver(
-        type_name = 'Mutation',
-        field_ame = 'removeCustomer',
-        request_mapping_template = appsync.MappingTemplate.dynamoDbDeleteItem('id', 'id'),
-        responseMappingTemplate = appsync.MappingTemplate.dynamo_db_result_item(),
+        # Mutation Resolver for deleting an existing Customer
+        customer_ds.create_resolver(
+            type_name='Mutation',
+            field_name='removeCustomer',
+            request_mapping_template=appsync.MappingTemplate.dynamo_db_delete_item('id', 'id'),
+            response_mapping_template=appsync.MappingTemplate.dynamo_db_result_item(),
         )
 
         # defines an AWS  Lambda resource
         loyalty_lambda = _lambda.Function(self, "LoyaltyLambdaHandler",
-                                            runtime=_lambda.Runtime.NODEJS_12_X,    # execution environment
-                                            handler="loyalty.handler",              # file is "loyalty", function is "handler"
-                                            code=_lambda.Code.from_asset("lambda"), # Code loaded from the lambda dir
-                                        )
+                                          runtime=_lambda.Runtime.NODEJS_12_X,
+                                          handler="loyalty.handler",
+                                          code=_lambda.Code.from_asset("lambda"),
+                                          )
 
         # Add Loyalty Lambda as a Datasource for the Graphql API.
-        loyaltyDS = api.addLambdaDataSource('Loyalty', 'The loyalty data source', loyalty_lambda)
+        loyalty_ds = api.add_lambda_data_source('Loyalty', 'The loyalty data source', loyalty_lambda)
 
         # Query Resolver to get all Customers
-        loyaltyDS.createResolver(
-        type_name = 'Query',
-        field_ame = 'getLoyaltyLevel',
-        request_mapping_template = appsync.MappingTemplate.lambdaRequest(),
-        responseMappingTemplate = appsync.MappingTemplate.lambdaResult(),
+        loyalty_ds.create_resolver(
+            type_name='Query',
+            field_name='getLoyaltyLevel',
+            request_mapping_template=appsync.MappingTemplate.lambda_request(),
+            response_mapping_template=appsync.MappingTemplate.lambda_result(),
         )
