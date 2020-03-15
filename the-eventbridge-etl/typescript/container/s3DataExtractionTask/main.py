3c934de9b872749a3c967fe3a2f5de68814c0a2e
Matt C <matthew.coulter@liberty-it.co.uk>
2020-03-15 18:09:24 +0000

    printing out csv reading to cloudwatch

--- the-eventbridge-etl/typescript/container/s3DataExtractionTask/main.py
+++ the-eventbridge-etl/typescript/container/s3DataExtractionTask/main.py
@@ -11,8 +11,11 @@ def main():
 
     if (None == data_s3_bucket_name
      or None == data_s3_object_key):
-        print('ERROR: unable to retrieve environment variables (s3 bucket or object ket, stream name')
+        print('ERROR: unable to retrieve environment variables (s3 bucket or object key, stream name')
         exit(1)
+
+    print('Bucket Name ' + data_s3_bucket_name)
+    print('S3 Object Key ' + data_s3_object_key)
 
     local_file = '/tmp/data.tsv'
 
@@ -20,6 +23,11 @@ def main():
     s3_resource.Object(data_s3_bucket_name, data_s3_object_key).download_file(local_file)
 
     print('SUCCESS: data file downloaded: ' + local_file)
+
+    with open(local_file) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(', '.join(row))
     exit(0)
 
 if __name__ == '__main__':
