d540d115618e5af3b5d226baf715309bf129c6a1
z8674558 <own7000hr@gmail.com>
2020-03-16 16:15:15 +0000

    s3 -> sqs started in python

--- the-eventbridge-etl/python/setup.py
+++ the-eventbridge-etl/python/setup.py
@@ -20,6 +20,11 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.28.0",
+        "aws-cdk.aws-lambda==1.28.0",
+        "aws-cdk.aws-dynamodb==1.28.0",
+        "aws-cdk.aws-s3==1.28.0",
+        "aws-cdk.aws-sqs==1.28.0",
+        "aws-cdk.aws-s3-notifications==1.28.0"
     ],
 
     python_requires=">=3.6",
