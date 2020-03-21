274f23b054f33b1c013f8d5463f5cd6d798eaf14
z8674558 <own7000hr@gmail.com>
2020-03-21 15:09:32 +0000

    trying prefix matching

--- the-destined-lambda/typescript/lambdas/destinedLambda.ts
+++ the-destined-lambda/typescript/lambdas/destinedLambda.ts
@@ -1,8 +1,8 @@
 exports.handler = async (event:any, context:any, callback:any) => {
     //throw new Error('test');
-    return JSON.stringify({
+    return {
       source: 'cdkpatterns.the-destined-lambda',
       action: 'message',
       message: 'hello world'
-    });
+    };
   };
