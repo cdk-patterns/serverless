73b5a2800fdae258d6d606ec6d4489ef76fca117
z8674558 <own7000hr@gmail.com>
2020-03-21 14:36:15 +0000

    trying refining rules

--- the-destined-lambda/typescript/lambdas/destinedLambda.ts
+++ the-destined-lambda/typescript/lambdas/destinedLambda.ts
@@ -1,17 +1,8 @@
 exports.handler = async (event:any, context:any, callback:any) => {
-    const response = {
-        // Event envelope fields
-        Source: 'cdkpatterns.the-destined-lambda',
-        EventBusName: 'default',
-        DetailType: 'event',
-        Time: new Date(),
-  
-        // Main event body
-        Detail: JSON.stringify({
-          action: 'message',
-          message: 'hello world'
-        })
-    };
     //throw new Error('test');
-    return "{'value': 'test'}";
+    return JSON.stringify({
+      source: 'cdkpatterns.the-destined-lambda',
+      action: 'message',
+      message: 'hello world'
+    });
   };
