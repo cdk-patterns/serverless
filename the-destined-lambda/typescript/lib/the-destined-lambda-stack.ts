69de53c676e4dfdd9b7c0a003bfd556d4f98bda0
z8674558 <own7000hr@gmail.com>
2020-03-21 15:44:56 +0000

    trying vtl to default mode for failure

--- the-destined-lambda/typescript/lib/the-destined-lambda-stack.ts
+++ the-destined-lambda/typescript/lib/the-destined-lambda-stack.ts
@@ -127,7 +127,7 @@ export class TheDestinedLambdaStack extends cdk.Stack {
           // Check: https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
           'application/json': "Action=Publish&"+
                               "TargetArn=$util.urlEncode('"+topic.topicArn+"')&"+
-                              "Message=[\"$input.params().querystring.get('mode')\"]&"+
+                              "Message=please $input.params().querystring.get('mode')&"+
                               "Version=2010-03-31"
         },
         passthroughBehavior: apigw.PassthroughBehavior.NEVER,
