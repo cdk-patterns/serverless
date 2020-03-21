9820892a7dac732a2b0525842627fb71361206b6
Matt C <matthew.coulter@liberty-it.co.uk>
2020-03-21 15:52:22 +0000

    adding failure handling logic to lambda

--- the-destined-lambda/typescript/lambdas/destinedLambda.ts
+++ the-destined-lambda/typescript/lambdas/destinedLambda.ts
@@ -1,10 +1,20 @@
 exports.handler = async (event:any, context:any, callback:any) => {
   console.log('Event Received')
   console.log(JSON.stringify(event));
-    //throw new Error('test');
-    return {
-      source: 'cdkpatterns.the-destined-lambda',
-      action: 'message',
-      message: 'hello world'
-    };
+
+  let records: any[] = event.Records;
+
+  //SNS can send multiple records
+  for(let index in records) {
+    let message = records[index]?.Sns.Message;
+    if(message == 'please fail'){
+      console.log('received failure flag, throwing error');
+      throw new Error('test');
+    }
+  }
+
+  return {
+    action: 'message',
+    message: 'hello world'
   };
+};
