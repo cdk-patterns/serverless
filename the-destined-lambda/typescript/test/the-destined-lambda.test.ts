41847ab464f0bf2cecfc56520a9438ccabb5e635
z8674558 <own7000hr@gmail.com>
2020-03-21 20:05:07 +0000

    adding tests

--- the-destined-lambda/typescript/test/the-destined-lambda.test.ts
+++ the-destined-lambda/typescript/test/the-destined-lambda.test.ts
@@ -1,13 +1,26 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheDestinedLambda = require('../lib/the-destined-lambda-stack');
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TheDestinedLambda.TheDestinedLambdaStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+test('EventBus Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDestinedLambda.TheDestinedLambdaStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Events::EventBus", {
+    "Name": "the-destined-lambda"
+  }
+  ));
+});
+
+test('SNS Topic Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheDestinedLambda.TheDestinedLambdaStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::SNS::Topic", {
+    "DisplayName": "The Destined Lambda CDK Pattern Topic"
+  }
+  ));
 });
