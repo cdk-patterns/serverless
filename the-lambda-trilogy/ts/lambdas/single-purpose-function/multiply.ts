741ff9699f0fb90eda19ad222e8db3667c3bf9ae
z8674558 <own7000hr@gmail.com>
2020-03-31 19:49:46 +0100

    start of new pattern

---
+++ the-lambda-trilogy/ts/lambdas/single-purpose-function/multiply.ts
@@ -0,0 +1,18 @@
+export {};
+
+exports.handler = async (event:any) => {
+    console.log(JSON.stringify(event, null, 2));
+
+    sendRes(200, "4");
+}
+
+const sendRes = (status:number, body:string) => {
+    var response = {
+      statusCode: status,
+      headers: {
+        "Content-Type": "text/html"
+      },
+      body: body
+    };
+    return response;
+  };
