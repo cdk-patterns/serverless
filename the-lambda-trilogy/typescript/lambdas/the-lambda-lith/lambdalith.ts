8ffc3d74489076a510d0103ef073914b45f0f7c1
z8674558 <own7000hr@gmail.com>
2020-03-31 20:48:50 +0100

    lambdalith add functionality

--- the-lambda-trilogy/typescript/lambdas/the-lambda-lith/lambdalith.ts
+++ the-lambda-trilogy/typescript/lambdas/the-lambda-lith/lambdalith.ts
@@ -12,7 +12,7 @@ function apiRoutes(){
         let result = Number(firstNum) + Number(secondNum);
         console.log(`result of ${firstNum} + ${secondNum} = ${result}`)
 
-        res.send(result)
+        res.send({"result":result})
     });
 
     return routes;
