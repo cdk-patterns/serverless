c496146efbad0503fae203b381c82f74384d163f
z8674558 <own7000hr@gmail.com>
2020-03-31 20:26:10 +0100

    express in a lambda

--- the-lambda-trilogy/typescript/lambdas/the-lambda-lith/lambdalith.ts
+++ the-lambda-trilogy/typescript/lambdas/the-lambda-lith/lambdalith.ts
@@ -1,5 +1,25 @@
 export {};
 
-exports.handler = async (event:any) => {
-    console.log(JSON.stringify(event, null, 2));
+const express = require('express');
+
+function apiRoutes(){
+    const routes = new express.Router();
+
+    routes.get('/v1/version', (req:any, res:any) => res.send({version: '1'}));
+
+    return routes;
+}
+
+
+const app = express()
+    .use(express.json())
+    .use(apiRoutes());
+
+const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
+if (isInLambda) {
+    const serverlessExpress = require('aws-serverless-express');
+    const server = serverlessExpress.createServer(app);
+    exports.main = (event:any, context:any) => serverlessExpress.proxy(server, event, context)
+} else {
+    app.listen(3000, () => console.log(`Listening on 3000`));
 }
