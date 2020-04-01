1ae2097066c150393399f4d791c2795a7277b68f
Matt C <matthew.coulter@liberty-it.co.uk>
2020-04-01 11:11:44 +0100

    add fat lambda stack to app

--- the-lambda-trilogy/python/app.py
+++ the-lambda-trilogy/python/app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from the_lambda_trilogy.the_lambda_lith_stack import TheLambdalithStack
+from the_lambda_trilogy.the_fat_lambda_stack import TheFatLambdaStack
 
 
 app = core.App()
 TheLambdalithStack(app, "the-lambda-lith-stack")
+TheFatLambdaStack(app, "the-fat-lambda-stack")
 
 app.synth()
