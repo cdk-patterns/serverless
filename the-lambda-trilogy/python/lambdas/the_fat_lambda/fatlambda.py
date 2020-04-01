68b3c900b3110d445b880fb6f27dedddd3d18ccb
Matt C <matthew.coulter@liberty-it.co.uk>
2020-04-01 11:19:24 +0100

    cast string to int

--- the-lambda-trilogy/python/lambdas/the_fat_lambda/fatlambda.py
+++ the-lambda-trilogy/python/lambdas/the_fat_lambda/fatlambda.py
@@ -2,10 +2,11 @@ def add(event, context):
     first_num = event["queryStringParameters"]['firstNum']
     if first_num is None:
         first_num = 0
+
     second_num = event["queryStringParameters"]['secondNum']
     if second_num is None:
         second_num = 0
 
-    result = first_num + second_num
+    result = int(first_num) + int(second_num)
     print("The result of % s + % s = %s" % (first_num, second_num, result))
     return {'body': result, 'statusCode': 200}
