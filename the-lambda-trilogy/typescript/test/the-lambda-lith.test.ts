df307c9995cf97b803ea2087d8ad8cf8358a93e3
Matt C <matthew.coulter@liberty-it.co.uk>
2020-04-02 11:07:06 +0100

    lambdalith tests added

--- the-lambda-trilogy/typescript/test/the-lambda-lith.test.ts
+++ the-lambda-trilogy/typescript/test/the-lambda-lith.test.ts
@@ -1,13 +1,27 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, haveResourceLike, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheLambdalithStack = require('../lib/the-lambda-lith-stack');
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TheLambdalithStack.TheLambdalithStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+test('Lambda-lith Lambda Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheLambdalithStack.TheLambdalithStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+    "Handler": "lambdalith.main",
+    "Runtime": "nodejs12.x"
+  }
+  ));
+});
+
+test('API Gateway Proxy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheLambdalithStack.TheLambdalithStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::Resource", {
+    "PathPart": "{proxy+}"
+  }
+  ));
 });
