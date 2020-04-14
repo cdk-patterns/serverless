10e0fd55705b0766db8edc802e69584d22ff5798
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2020-04-14 15:03:28 +0100

    start of SAGA pattern

---
+++ the-saga-stepfunction/python/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="the_saga_stepfunction",
+    version="0.0.1",
+
+    description="An empty CDK Python app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="author",
+
+    package_dir={"": "the_saga_stepfunction"},
+    packages=setuptools.find_packages(where="the_saga_stepfunction"),
+
+    install_requires=[
+        "aws-cdk.core==1.32.2",
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: Apache Software License",
+
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+
+        "Typing :: Typed",
+    ],
+)
