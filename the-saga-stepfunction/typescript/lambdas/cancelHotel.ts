8f8466d5ca6be6604b432e12000ab6cd6492da4c
z8674558 <own7000hr@gmail.com>
2020-04-15 11:18:32 +0100

    base implementation added for cancel lambdas

--- the-saga-stepfunction/typescript/lambdas/cancelHotel.ts
+++ the-saga-stepfunction/typescript/lambdas/cancelHotel.ts
@@ -1,5 +1,39 @@
+const { DynamoDB } = require('aws-sdk');
 export {};
 
-exports.handler = async (event:any) => {
-    console.log(JSON.stringify(event, null, 2));
-}
+/* input example:
+ *  { trip_id: some_guid,
+ *    depart: london,
+ *    depart_at: some_date,
+ *    arrive: dublin,
+ *    arrive_at: some_date,
+ *    hotel: holiday inn,
+ *    check_in: some_date,
+ *    check_out: some_date,
+ *    rental: volvo,
+ *    rental_from: some_date,
+ *    rental_to: some_date
+ *  }
+ */
+exports.handler = async function(event:any) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+
+  // create AWS SDK clients
+  const dynamo = new DynamoDB();
+
+  var params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      'trip_id' : {S: event.trip_id}
+    }
+  };
+  
+  // Call DynamoDB to add the item to the table
+  let result = await dynamo.deleteItem(params).promise();
+
+  console.log('deleted hotel booking:');
+  console.log(result);
+
+  // return status of ok
+  return {status: "ok"}
+};
