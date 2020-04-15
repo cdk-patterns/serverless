7751d8337ff54d670712633ee4f8b7df4022d5c2
z8674558 <own7000hr@gmail.com>
2020-04-15 11:48:40 +0100

    update runType to run_type

--- the-saga-stepfunction/typescript/lambdas/bookHotel.ts
+++ the-saga-stepfunction/typescript/lambdas/bookHotel.ts
@@ -19,7 +19,7 @@ exports.handler = async function(event:any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
   // If we passed the parameter to fail this step 
-  if(event.runType === 'failHotel'){
+  if(event.run_type === 'failHotel'){
       throw new Error('Failed to book the hotel');
   }
 
