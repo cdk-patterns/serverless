50d8089bb106cc2f86d55cf8df9fc94a99c4cdc3
z8674558 <own7000hr@gmail.com>
2020-04-16 16:21:19 +0100

    removing multi table stack

--- the-saga-stepfunction/typescript/bin/the-saga-stepfunction.ts
+++ the-saga-stepfunction/typescript/bin/the-saga-stepfunction.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { TheSagaStepfunctionStack } from '../lib/the-saga-stepfunction-stack';
 import { TheSagaStepfunctionSingleTableStack } from '../lib/the-saga-stepfunction-single-table-stack';
 
 const app = new cdk.App();
-new TheSagaStepfunctionStack(app, 'TheSagaStepfunctionStack');
 new TheSagaStepfunctionSingleTableStack(app, 'TheSagaStepfunctionSingleTableStack');
