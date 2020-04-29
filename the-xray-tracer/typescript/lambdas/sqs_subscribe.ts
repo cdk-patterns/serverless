e39ab4c941f0b542cf1d7e6cc6fcbc211bcf5ba6
Matt C <matthew.coulter@liberty-it.co.uk>
2020-04-29 20:08:23 +0100

    pull the trace id from the attributes

--- the-xray-tracer/typescript/lambdas/sqs_subscribe.ts
+++ the-xray-tracer/typescript/lambdas/sqs_subscribe.ts
@@ -1,9 +1,18 @@
+export {};
+const AWSXRay = require('aws-xray-sdk');
+
 exports.handler = async function(event:any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
   let records: any[] = event.Records;
   
   for(let index in records) {
+
+    let traceHeaderStr = records[index].attributes.AWSTraceHeader;
+    const traceData = AWSXRay.utils.processTraceData(traceHeaderStr);
+    const segment = new AWSXRay.Segment("SQS", traceData.Root, traceData.Parent);
     console.log('received message ' + records[index].body);  
+
+    segment.addMetadata("message", records[index].body)
   }
 };
