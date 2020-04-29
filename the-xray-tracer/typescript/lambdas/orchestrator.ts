31f5f23e70177c7233d6033d4dff2f86e173600b
Matt C <matthew.coulter@liberty-it.co.uk>
2020-04-29 15:55:51 +0100

    trying to build a lambda chain to make x-ray more interesting

--- the-xray-tracer/typescript/lambdas/orchestrator.ts
+++ the-xray-tracer/typescript/lambdas/orchestrator.ts
@@ -8,8 +8,22 @@ exports.handler = async function(event:any) {
         FunctionName: process.env.DYNAMO_FN_ARN, 
         Payload: JSON.stringify({path:"test"}), 
        };
-    lambda.invoke(params, function(err:any, data:any) {
+    await lambda.invoke(params, function(err:any, data:any) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
-    });
+    }).promise();
+
+    // return response back to upstream caller
+  return sendRes(200, 'You have connected with the Lambda!');
 }
+
+const sendRes = (status:number, body:string) => {
+    var response = {
+      statusCode: status,
+      headers: {
+        "Content-Type": "text/html"
+      },
+      body: body
+    };
+    return response;
+  };
