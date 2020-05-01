fad105b6329a1c59d58b6490cc1135ed14a2523d
z8674558 <own7000hr@gmail.com>
2020-05-01 11:19:51 +0100

    breaking up the mono stack into tech specific flow stacks

--- the-xray-tracer/typescript/test/the-xray-tracer.test.ts
+++ the-xray-tracer/typescript/test/the-xray-tracer.test.ts
@@ -1,11 +1,23 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as TheXrayTracer from '../lib/the-xray-tracer-stack';
+import lambda = require('@aws-cdk/aws-lambda'); 
 
 test('Empty Stack', () => {
     const app = new cdk.App();
+
+    let mockFunction = new lambda.Function(app, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.inline(''),
+      handler: 'test'
+    });
+
     // WHEN
-    const stack = new TheXrayTracer.TheXrayTracerStack(app, 'MyTestStack');
+    const stack = new TheXrayTracer.TheXrayTracerStack(app, 'MyTestStack', {
+      dynamoFlowLambda: mockFunction,
+      httpFlowLambda: mockFunction,
+      sqsFlowLambda: mockFunction
+  });
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
