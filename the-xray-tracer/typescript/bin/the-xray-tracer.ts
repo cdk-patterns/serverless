d530ba3010da1ea0e300e5aa4caa52a9674b97b9
Matt C <matthew.coulter@liberty-it.co.uk>
2020-05-01 13:46:46 +0100

    sns route added

--- the-xray-tracer/typescript/bin/the-xray-tracer.ts
+++ the-xray-tracer/typescript/bin/the-xray-tracer.ts
@@ -5,18 +5,22 @@ import { TheXrayTracerStack } from '../lib/the-xray-tracer-stack';
 import { TheDynamoFlowStack } from '../lib/the-dynamo-flow-stack';
 import { TheHttpFlowStack } from '../lib/the-http-flow-stack';
 import { TheSqsFlowStack } from '../lib/the-sqs-flow-stack';
+import { TheSnsFlowStack } from '../lib/the-sns-flow-stack';
 
 const app = new cdk.App();
 let dynamoFlow = new TheDynamoFlowStack(app, 'TheXrayDynamoFlow');
 let httpFlow = new TheHttpFlowStack(app, 'TheXrayHttpFlow');
 let sqsFlow = new TheSqsFlowStack(app, 'TheXraySQSFlow');
+let snsFlow = new TheSnsFlowStack(app, 'TheXraySnsFlow');
 
 let xrayStack = new TheXrayTracerStack(app, 'TheXrayTracerStack', {
     dynamoFlowLambda: dynamoFlow.dynamoLambda,
     httpFlowLambda: httpFlow.httpLambda,
-    sqsFlowLambda: sqsFlow.sqslambda
+    sqsFlowLambda: sqsFlow.sqslambda,
+    snsFlowLambda: snsFlow.snsLambda
 });
 
 xrayStack.addDependency(dynamoFlow, 'needs the lambda to trigger the DynamoDB flow');
 xrayStack.addDependency(httpFlow, 'needs the lambda to trigger the http flow');
-xrayStack.addDependency(sqsFlow, 'needs the lambda to trigger the sqs flow')
+xrayStack.addDependency(sqsFlow, 'needs the lambda to trigger the sqs flow');
+xrayStack.addDependency(snsFlow, 'needs the lambda to trigger the sns flow')
---
+++ the-xray-tracer/typescript/lib/the-sns-flow-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import sqs = require('@aws-cdk/aws-sqs');
+import sns = require('@aws-cdk/aws-sns');
+import sns_sub = require('@aws-cdk/aws-sns-subscriptions');
+
+export class TheSnsFlowStack extends cdk.Stack {
+    snsLambda: lambda.Function;
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        /**
+         * SNS Topic Creation
+         * Our API Gateway posts messages directly to this
+         */
+        const topic = new sns.Topic(this, 'TheXRayTracerSnsTopic', {
+                displayName: "The XRay Tracer CDK Pattern Topic",
+        });
+
+        // defines an AWS Lambda resource
+        this.snsLambda = new lambda.Function(this, 'snsLambdaHandler', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            code: lambda.Code.asset('lambdas'),
+            handler: 'sns_publish.handler',
+            environment: {
+                TOPIC_ARN: topic.topicArn
+            },
+            tracing: lambda.Tracing.ACTIVE
+        });
+        topic.grantPublish(this.snsLambda);
+
+        // SNS Subscriber Queue
+        const createdStatusQueue = new sqs.Queue(this, 'XRayTracerSnsSubscriberQueue', {
+            visibilityTimeout: cdk.Duration.seconds(300),
+            queueName: 'XRayTracerSnsSubscriberQueue',
+        });
+  
+        // subscribe SQS to our SNS Topic
+        topic.addSubscription(new sns_sub.SqsSubscription(createdStatusQueue, {
+            rawMessageDelivery: true
+        }));
+    }
+}
