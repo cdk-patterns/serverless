9d2af3c69216ca602bb70a34eef86cf17dbf74a0
Matt C <matthew.coulter@liberty-it.co.uk>
2020-05-02 14:50:15 +0100

    trying reverse dependency

--- the-xray-tracer/typescript/lib/the-http-flow-stack.ts
+++ the-xray-tracer/typescript/lib/the-http-flow-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 import lambda = require('@aws-cdk/aws-lambda');
+import sns_sub = require('@aws-cdk/aws-sns-subscriptions');
+import sns = require('@aws-cdk/aws-sns');
+
+export interface HttpFlowStackProps extends cdk.StackProps{
+    readonly snsTopicARN: string;
+}
 
 export class TheHttpFlowStack extends cdk.Stack {
 
     httpLambda: lambda.Function;
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: cdk.Construct, id: string, props: HttpFlowStackProps) {
         super(scope, id, props);
 
         // defines an AWS Lambda resource
@@ -14,5 +20,8 @@ export class TheHttpFlowStack extends cdk.Stack {
             handler: 'http.handler',
             tracing: lambda.Tracing.ACTIVE
         });
+
+        let topic = sns.Topic.fromTopicArn(this, 'SNSTopic', props.snsTopicARN);
+        topic.addSubscription(new sns_sub.LambdaSubscription(this.httpLambda));
     }
 }
