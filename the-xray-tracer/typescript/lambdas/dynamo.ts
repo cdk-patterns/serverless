cb557c41dd54c3586dd1b187dcecbc41ad0ed32a
Matt C <matthew.coulter@liberty-it.co.uk>
2020-05-02 15:37:07 +0100

    refactor lambdas to pull path off sns message

--- the-xray-tracer/typescript/lambdas/dynamo.ts
+++ the-xray-tracer/typescript/lambdas/dynamo.ts
@@ -16,7 +16,7 @@ exports.handler = async function(event:any) {
   // update dynamo entry for "path" with hits++
   await dynamo.updateItem({
     TableName: process.env.HITS_TABLE_NAME,
-    Key: { path: { S: event.path } },
+    Key: { path: { S: path } },
     UpdateExpression: 'ADD hits :incr',
     ExpressionAttributeValues: { ':incr': { N: '1' } }
   }).promise();
