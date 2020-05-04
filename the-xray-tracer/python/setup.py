8c4f1a2952d59a0811ebdeeb574c2cc0eef7a1f4
z8674558 <own7000hr@gmail.com>
2020-05-04 15:45:58 +0100

    api gateway and sns topic added

--- the-xray-tracer/python/setup.py
+++ the-xray-tracer/python/setup.py
@@ -20,6 +20,9 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.36.1",
+        "aws-cdk.aws-iam==1.36.1",
+        "aws-cdk.aws_apigateway==1.36.1",
+        "aws-cdk.aws-sns==1.36.1",
     ],
 
     python_requires=">=3.6",
