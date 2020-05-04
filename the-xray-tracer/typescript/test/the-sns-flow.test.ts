e45deefa095036204d33b1ac78457c32fae5bc89
z8674558 <own7000hr@gmail.com>
2020-05-04 14:18:19 +0100

    add in sns flow tests

--- the-xray-tracer/typescript/test/the-sns-flow.test.ts
+++ the-xray-tracer/typescript/test/the-sns-flow.test.ts
@@ -2,6 +2,44 @@ import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheSnsFlow = require('../lib/the-sns-flow-stack');
 
+test('SNS Topic Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheSnsFlow.TheSnsFlowStack(app, 'TheSnsFlowStack', {
+        snsTopicARN: 'arn:1234:1234:1234:1234:1234'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::SNS::Topic", {
+    }));
+  });
+
+
+test('SNS Publish IAM Policy Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheSnsFlow.TheSnsFlowStack(app, 'TheSnsFlowStack', {
+        snsTopicARN: 'arn:1234:1234:1234:1234:1234'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+        "PolicyDocument": {
+        "Statement": [
+            {
+                "Action": [
+                  "xray:PutTraceSegments",
+                  "xray:PutTelemetryRecords"
+                ],
+                "Effect": "Allow"
+            },
+            {
+                "Action": "sns:Publish",
+                "Effect": "Allow"
+            }]
+        }
+    }
+    ));
+});
+
 test('SNS Publish Lambda Created - Tracing Enabled', () => {
     const app = new cdk.App();
     // WHEN
@@ -19,7 +57,37 @@ test('SNS Publish Lambda Created - Tracing Enabled', () => {
       ));
   });
 
-  test('SNS Subscribe Lambda Created - Tracing Enabled', () => {
+test('SNS Lambda Subscription Created', () => {
+    const app = new cdk.App();
+    const topicARN = 'arn:1234:1234:1234:1234:1234';
+    // WHEN
+    const stack = new TheSnsFlow.TheSnsFlowStack(app, 'TheSnsFlowStack', {
+        snsTopicARN: topicARN
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::SNS::Subscription", {
+        "Protocol": "lambda",
+        "TopicArn": topicARN
+    }
+    ));
+});
+
+test('SNS Given Lambda Invoke Permissions', () => {
+    const app = new cdk.App();
+    const topicARN = 'arn:1234:1234:1234:1234:1234';
+    // WHEN
+    const stack = new TheSnsFlow.TheSnsFlowStack(app, 'TheSnsFlowStack', {
+        snsTopicARN: topicARN
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::Lambda::Permission", {
+        "Action": "lambda:InvokeFunction",
+        "Principal": "sns.amazonaws.com"
+    }
+    ));
+});
+
+test('SNS Subscribe Lambda Created - Tracing Enabled', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new TheSnsFlow.TheSnsFlowStack(app, 'TheSnsFlowStack', {
@@ -34,4 +102,4 @@ test('SNS Publish Lambda Created - Tracing Enabled', () => {
         }
       }
       ));
-  });
+});
