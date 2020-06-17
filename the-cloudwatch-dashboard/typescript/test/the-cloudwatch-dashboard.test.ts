4487d7857d44c1cb48ed822c7157bf51931c5853
Matt C <matthew.coulter@liberty-it.co.uk>
2020-06-17 10:50:44 +0100

    added simple test for dashboard, need to work out non-brittle test technique

--- the-cloudwatch-dashboard/typescript/test/the-cloudwatch-dashboard.test.ts
+++ the-cloudwatch-dashboard/typescript/test/the-cloudwatch-dashboard.test.ts
@@ -76,6 +76,15 @@ test('SNS Topic Created', () => {
   expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
 });
 
+test('CloudWatch Dashboard Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::CloudWatch::Dashboard"));
+});
+
+
 test('% API Gateway 4xx Errors Alarm Created', () => {
   const app = new cdk.App();
   // WHEN
@@ -103,12 +112,13 @@ test('% API Gateway 4xx Errors Alarm Created', () => {
           "Namespace": "AWS/ApiGateway"
         }
       }
-    }]
+    }],
+    "Threshold": 1
   }
   ));
 });
 
-test('% API Gateway 5xx Errors Alarm Created', () => {
+test('>0 API Gateway 5xx Errors Alarm Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
@@ -116,6 +126,166 @@ test('% API Gateway 5xx Errors Alarm Created', () => {
   expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
     "MetricName": "5XXError",
     "Namespace": "AWS/ApiGateway",
+    "Threshold": 0
+  }
+  ));
+});
+
+test('p99 Latency > 1 second API Gateway Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "MetricName": "Latency",
+    "ExtendedStatistic": "p99",
+    "Namespace": "AWS/ApiGateway",
+    "Threshold": 1000
+  }
+  ));
+});
+
+test('% of invocations that errored, last 5 mins Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "Metrics": [{
+      "Expression": "e / i * 100",
+      "Label": "% of invocations that errored, last 5 mins"
+    }, 
+    {
+      "Id": "i",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "Invocations",
+          "Namespace": "AWS/Lambda"
+        }
+      }
+    },
+    {
+      "Id": "e",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "Errors",
+          "Namespace": "AWS/Lambda"
+        }
+      }
+    }],
+    "Threshold": 2
+  }
+  ));
+});
+
+test('Dynamo Lambda p99 Long Duration Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "MetricName": "Duration",
+    "ExtendedStatistic": "p99",
+    "Namespace": "AWS/Lambda",
+    "Threshold": 1000
+  }
+  ));
+});
+
+test('% of throttled requests, last 30 mins Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "Metrics": [{
+      "Expression": "t / (i + t) * 100",
+      "Label": "% of throttled requests, last 30 mins"
+    }, 
+    {
+      "Id": "i",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "Invocations",
+          "Namespace": "AWS/Lambda"
+        }
+      }
+    },
+    {
+      "Id": "t",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "Throttles",
+          "Namespace": "AWS/Lambda"
+        }
+      }
+    }],
+    "Threshold": 2
+  }
+  ));
+});
+
+test('DynamoDB Throttles Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "Metrics": [{
+      "Expression": "m1 + m2",
+      "Label": "DynamoDB Throttles"
+    }, 
+    {
+      "Id": "m1",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "ReadThrottleEvents",
+          "Namespace": "AWS/DynamoDB"
+        }
+      }
+    },
+    {
+      "Id": "m2",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "WriteThrottleEvents",
+          "Namespace": "AWS/DynamoDB"
+        }
+      }
+    }],
+    "Threshold": 1
+  }
+  ));
+});
+
+test('DynamoDB Errors Alarm Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TheCloudwatchDashboard.TheCloudwatchDashboardStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::CloudWatch::Alarm", {
+    "Metrics": [{
+      "Expression": "m1 + m2",
+      "Label": "DynamoDB Errors"
+    }, 
+    {
+      "Id": "m1",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "UserErrors",
+          "Namespace": "AWS/DynamoDB"
+        }
+      }
+    },
+    {
+      "Id": "m2",
+      "MetricStat": {
+        "Metric": {
+          "MetricName": "SystemErrors",
+          "Namespace": "AWS/DynamoDB"
+        }
+      }
+    }],
     "Threshold": 0
   }
   ));
