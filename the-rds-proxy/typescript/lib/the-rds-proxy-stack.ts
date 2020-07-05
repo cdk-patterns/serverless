7de3503b8fe9fc906a7c6087bf3424957d3e9168
z8674558 <own7000hr@gmail.com>
2020-07-05 09:27:03 +0000

    working rds proxy

--- the-rds-proxy/typescript/lib/the-rds-proxy-stack.ts
+++ the-rds-proxy/typescript/lib/the-rds-proxy-stack.ts
@@ -2,7 +2,10 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as rds from '@aws-cdk/aws-rds';
 import * as secrets from '@aws-cdk/aws-secretsmanager';
+const ssm = require('@aws-cdk/aws-ssm');
 import * as lambda from '@aws-cdk/aws-lambda';
+import apigw = require('@aws-cdk/aws-apigatewayv2');
+import iam = require('@aws-cdk/aws-iam');
 
 export class TheRdsProxyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,41 +14,87 @@ export class TheRdsProxyStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, 'Vpc', {
       maxAzs: 2, // Default is all AZs in the region
     });
-
-    new ec2.SecurityGroup(this, 'securityGroup', {
+    
+    let lambdaToDBGroup = new ec2.SecurityGroup(this, 'Lambda to DB Connection', {
       vpc
-    })
-    const secret = new secrets.Secret(this, 'secret');
+    });
+    let dbConnectionGroup = new ec2.SecurityGroup(this, 'Proxy to DB Connection', {
+      vpc
+    });
+    dbConnectionGroup.addIngressRule(dbConnectionGroup, ec2.Port.tcp(3306), 'allow db connection');
+    dbConnectionGroup.addIngressRule(lambdaToDBGroup, ec2.Port.tcp(3306), 'allow lambda connection');
 
-    // Create a MySQL DB
-    const rdsInstance = new rds.DatabaseInstance(this, 'Instance', {
+    const databaseUsername = 'syscdk';
+
+    const databaseCredentialsSecret = new secrets.Secret(this, 'DBCredentialsSecret', {
+      secretName: 'rds-credentials',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: databaseUsername,
+        }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'password'
+      }
+    });
+
+    new ssm.StringParameter(this, 'DBCredentialsArn', {
+      parameterName: 'rds-credentials-arn',
+      stringValue: databaseCredentialsSecret.secretArn,
+    });
+
+    const rdsInstance = new rds.DatabaseInstance(this, 'DBInstance', {
       engine: rds.DatabaseInstanceEngine.MYSQL,
+      masterUsername: databaseCredentialsSecret.secretValueFromJson('username').toString(),
+      masterUserPassword: databaseCredentialsSecret.secretValueFromJson('password'),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
-      masterUsername: 'syscdk',
-      vpc
+      vpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      securityGroups: [dbConnectionGroup]
     });
 
     // Create an RDS Proxy
     const proxy = rdsInstance.addProxy('proxy', {
-        borrowTimeout: cdk.Duration.seconds(30),
-        maxConnectionsPercent: 50,
-        secret,
+        secret: databaseCredentialsSecret,
+        debugLogging: true,
         vpc,
+        securityGroups: [dbConnectionGroup]
     });
+    
+    // Workaround for bug where TargetGroupName is not set but required
+    let targetGroup = proxy.node.children.find((child:any) => {
+      return child instanceof rds.CfnDBProxyTargetGroup
+    }) as rds.CfnDBProxyTargetGroup
 
+    targetGroup.addPropertyOverride('TargetGroupName', 'default');
+    
     // Lambda to Interact with RDS Proxy
     const rdsLambda = new lambda.Function(this, 'extractLambdaHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.asset('lambdas/rds'), 
       handler: 'rdsLambda.handler',
       vpc: vpc,
+      securityGroups: [lambdaToDBGroup],
       environment: {
         PROXY_ENDPOINT: proxy.endpoint,
-        PROXY_SECRET: secret.secretValue.toString()
+        RDS_SECRET_NAME: 'rds-credentials',
+        NODE_OPTIONS: '--tls-min-v1.0'
       }
     });
 
-    // Allow our lambda function to connect to our proxy
-    proxy.connections.allowFrom(rdsLambda, ec2.Port.allTraffic())
+    databaseCredentialsSecret.grantRead(rdsLambda);
+
+    // defines an API Gateway Http API resource backed by our "rdsLambda" function.
+    let api = new apigw.HttpApi(this, 'Endpoint', {
+      defaultIntegration: new apigw.LambdaProxyIntegration({
+        handler: rdsLambda
+      })
+    });
+
+   new cdk.CfnOutput(this, 'HTTP API Url', {
+     value: api.url ?? 'Something went wrong with the deploy'
+   });
+
   }
 }
