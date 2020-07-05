7a36682cfa5759387a5e443efc631cb6dcc4b702
z8674558 <own7000hr@gmail.com>
2020-07-05 20:12:43 +0100

    make npm i for lambda folder automatic

--- the-rds-proxy/python/app.py
+++ the-rds-proxy/python/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
+import subprocess
 from aws_cdk import core
 
 from the_rds_proxy.the_rds_proxy_stack import TheRdsProxyStack
 
+# install node dependencies for lambdas
+subprocess.check_call("npm i".split(), cwd="lambdas/rds")
 
 app = core.App()
 TheRdsProxyStack(app, "the-rds-proxy")
