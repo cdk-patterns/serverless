729c762d79f44973453761585319310ba72e9d9e
z8674558 <own7000hr@gmail.com>
2020-07-09 15:26:46 +0000

    start of polly pattern

---
+++ polly/typescript/lib/polly-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigatewayv2');
+import iam = require('@aws-cdk/aws-iam');
+
+export class PollyStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pollyLambda = new lambda.Function(this, 'PollyHandler', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      code: lambda.Code.asset('lambdas'), 
+      handler: 'polly.handler',
+      environment: {
+      }
+    });
+    
+    const pollyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'polly:SynthesizeSpeech'
+      ]
+    });
+    pollyLambda.addToRolePolicy(pollyStatement);
+
+    // defines an API Gateway Http API resource backed by our "pollyLambda" function.
+    let api = new apigw.HttpApi(this, 'Endpoint', {
+      defaultIntegration: new apigw.LambdaProxyIntegration({
+        handler: pollyLambda
+      })
+    });
+
+   new cdk.CfnOutput(this, 'HTTP API Url', {
+     value: api.url ?? 'Something went wrong with the deploy'
+   });
+  }
+}
