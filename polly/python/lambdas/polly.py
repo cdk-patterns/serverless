8b0401e0186102ffcafa46e81c9d3b10b9b42544
Matt C <matthew.coulter@liberty-it.co.uk>
2020-07-10 21:50:06 +0100

    add translate into python impl

--- polly/python/lambdas/polly.py
+++ polly/python/lambdas/polly.py
@@ -3,18 +3,32 @@ import sys
 import base64
 
 polly_c = boto3.client('polly')
+translate_c = boto3.client('translate')
 
 def handler(event, context):
     try:
         voice = event["queryStringParameters"]["voice"]
     except KeyError:
         voice = 'Matthew'
+
+    try:
+        translate_from = event["queryStringParameters"]["translateFrom"]
+    except KeyError:
+        translate_from = 'en'
+
+    try:
+        translate_to = event["queryStringParameters"]["translateTo"]
+    except KeyError:
+        translate_to = 'en'
         
     try:
         text = event['body']
     except KeyError:
         text = 'To hear your own script, you need to include text in the message body of your restful request to the API Gateway'
         
+    if translate_to != translate_from:
+        text = translate_text(text, translate_from, translate_to)
+
     speech = convert_text_to_speech(voice, text)
     
     return {
@@ -23,6 +37,15 @@ def handler(event, context):
         'body': base64.b64encode(speech),
         'isBase64Encoded': True
     }
+
+def translate_text(text, translate_from, translate_to):
+    response = translate_c.translate_text(
+        Text=text,
+        SourceLanguageCode=translate_from,
+        TargetLanguageCode=translate_to
+    )
+
+    return response['TranslatedText']
 
 def convert_text_to_speech(voice, text):
     if voice not in ['Joanna', 'Matthew', 'Lupe']:
