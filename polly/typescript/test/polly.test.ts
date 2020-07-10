87dd50dd0257e55bd186d0add79793cd9126a55c
z8674558 <own7000hr@gmail.com>
2020-07-10 08:53:47 +0100

    tests added

--- polly/typescript/test/polly.test.ts
+++ polly/typescript/test/polly.test.ts
@@ -1,13 +1,44 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Polly from '../lib/polly-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Polly.PollyStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Polly Lambda Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Polly.PollyStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+    "Handler": "polly.handler",
+    "Runtime": "python3.8"
+  }
+  ));
+});
+
+test('Polly IAM Policy Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Polly.PollyStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+    "PolicyDocument": {
+      "Statement": [
+        {
+        "Action": "polly:SynthesizeSpeech",
+        "Effect": "Allow",
+        "Resource": "*"
+      }]
+    }
+  }
+  ));
+});
+
+test('API Gateway Http API Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Polly.PollyStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGatewayV2::Api", {
+    "ProtocolType": "HTTP"
+  }
+  ));
 });
