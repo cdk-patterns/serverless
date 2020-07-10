f0a8b61812157f0f0dba7b440a10d6efbde08d7e
Matt C <matthew.coulter@liberty-it.co.uk>
2020-07-10 08:30:11 +0100

    add lambda into python version

---
+++ polly/python/lambdas/polly.py
@@ -0,0 +1,38 @@
+import boto3
+import sys
+import base64
+
+polly_c = boto3.client('polly')
+
+def handler(event, context):
+    try:
+        voice = event["queryStringParameters"]["voice"]
+    except KeyError:
+        voice = 'Matthew'
+        
+    try:
+        text = event['body']
+    except KeyError:
+        text = 'you need to include text in your message body'
+        
+    speech = convert_text_to_speech(voice, text)
+    
+    return {
+        'statusCode': 200,
+        'headers': { 'Content-Type': 'audio/mpeg' },
+        'body': base64.b64encode(speech),
+        'isBase64Encoded': True
+    }
+
+def convert_text_to_speech(voice, text):
+    if voice not in ['Joanna', 'Matthew', 'Lupe']:
+        print('Only Joanna, Matthew and Lupe support the newscaster style')
+        sys.exit(1)
+    response = polly_c.synthesize_speech(
+                   VoiceId=voice,
+                   Engine='neural',
+                   OutputFormat='mp3',
+                   TextType='ssml',
+                   Text = f'<speak><amazon:domain name="news">{text}></amazon:domain></speak>')
+
+    return response['AudioStream'].read()
