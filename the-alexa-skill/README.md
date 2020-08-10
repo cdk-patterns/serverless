768bcea82c78332d0c7361ddeeaa26735a95fa22
Chris Plankey <plankey5@aol.com>
2020-08-09 21:06:05 -0400

    first pass at tests and readme

---
+++ the-alexa-skill/README.md
@@ -0,0 +1,16 @@
+# The Alexa Skill
+
+This deploys two CDK stacks that produce an Alexa Skill backed by a Lambda Function.
+
+## Prerequisites:
+* An Amazon Developer account from: https://developer.amazon.com/
+* A developer account security profile: https://developer.amazon.com/loginwithamazon/console/site/lwa/create-security-profile.html
+* An AWS Account with CLI Access
+
+## File Structure
+
+
+
+## Available Versions
+
+ * [TypeScript](typescript/)
--- the-alexa-skill/typescript/test/the-alexa-skill.test.ts
+++ the-alexa-skill/typescript/test/the-alexa-skill.test.ts
@@ -1,5 +1,165 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import {
+    expect as expectCDK,
+    haveResourceLike
+} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as TheAlexaSkill from '../lib/the-alexa-skill-stack';
+import TheAlexaSkill = require('../lib/the-alexa-skill-stack');
+import TheAssetStack = require('../lib/the-asset-stack');
 
+//The Alexa Skill
+test('IAM Role to Access S3 Assets', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::IAM::Role", {
+        "AssumeRolePolicyDocument": {
+            "Statement": [{
+                "Action": "sts:AssumeRole",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": [
+                        "alexa-appkit.amazon.com",
+                        "cloudformation.amazonaws.com"
+                    ]
+                }
+            }],
+            "Version": "2012-10-17"
+        }
+    }));
+});
 
+test('IAM Policy to Access S3 Assets', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+        "PolicyDocument": {
+            "Statement": [{
+                "Action": "S3:*",
+                "Effect": "Allow",
+                "Resource": [
+                    "bar",
+                    "bar/*"
+                ]
+            }]
+        }
+    }));
+});
+
+test('DynamoDB Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::DynamoDB::Table", {
+        "KeySchema": [{
+            "AttributeName": "userId",
+            "KeyType": "HASH"
+        }],
+    }));
+});
+
+test('Lambda Backend for Alexa', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+        "Runtime": "nodejs12.x",
+        "Environment": {
+            "Variables": {
+                "USERS_TABLE_NAME": {
+                    "Ref": "Users0A0EEA89"
+                }
+            }
+        }
+    }));
+});
+
+test('DynamoDB Read/Write IAM Policy Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+        "PolicyDocument": {
+            "Statement": [{
+                "Action": [
+                    "dynamodb:BatchGetItem",
+                    "dynamodb:GetRecords",
+                    "dynamodb:GetShardIterator",
+                    "dynamodb:Query",
+                    "dynamodb:GetItem",
+                    "dynamodb:Scan",
+                    "dynamodb:BatchWriteItem",
+                    "dynamodb:PutItem",
+                    "dynamodb:UpdateItem",
+                    "dynamodb:DeleteItem"
+                ],
+                "Effect": "Allow"
+            }]
+        }
+    }));
+});
+test('Alexa Skill Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("Alexa::ASK::Skill", {
+        "SkillPackage": {
+            "S3Bucket": "foo",
+            "S3Key": "skill.zip"
+        }
+    }));
+});
+
+test('Lambda Permission for Alexa', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAlexaSkill.TheAlexaSkillStack(app, 'MyTestStack', {
+        assetBucketName: 'foo',
+        assetBucketARN: 'bar'
+    });
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::Lambda::Permission", {
+        "Action": "lambda:InvokeFunction",
+        "Principal": "alexa-appkit.amazon.com"
+    }));
+});
+
+///The Asset Stack
+test('S3 Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAssetStack.TheAssetStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(haveResourceLike("AWS::S3::Bucket"));
+});
+test('S3 Deployment Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new TheAssetStack.TheAssetStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(haveResourceLike("Custom::CDKBucketDeployment"));
+});
