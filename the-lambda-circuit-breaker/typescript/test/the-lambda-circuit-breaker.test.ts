7b2a1e59a4202f9ea69f1905fb7f9a9e5e73f73a
Matt C <matthew.coulter@liberty-it.co.uk>
2020-10-14 16:42:37 +0100

    tests working

--- the-lambda-circuit-breaker/typescript/test/the-lambda-circuit-breaker.test.ts
+++ the-lambda-circuit-breaker/typescript/test/the-lambda-circuit-breaker.test.ts
@@ -2,78 +2,60 @@ import { expect as expectCDK, matchTemplate, haveResourceLike,MatchStyle } from 
 import * as cdk from '@aws-cdk/core';
 import * as TheLambdaCircuitBreaker from '../lib/the-lambda-circuit-breaker-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+let stack:TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack;
+
+beforeAll(() => {
+  const app = new cdk.App();
+  // WHEN
+  stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
 });
 
 test('DynamoDB Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
-  // THEN
-expectCDK(stack).to(haveResourceLike("AWS::DynamoDB::Table", {
-  "KeySchema": [
-    {
-      "AttributeName": "id",
-      "KeyType": "HASH"
-    }
-  ]}
-));
+  expectCDK(stack).to(haveResourceLike("AWS::DynamoDB::Table", {
+    "KeySchema": [
+      {
+        "AttributeName": "id",
+        "KeyType": "HASH"
+      }
+    ]}
+  ));
 });
 
 test('DynamoDB Read/Write IAM Policy Created', () => {
-const app = new cdk.App();
-// WHEN
-const stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
-// THEN
-expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
-  "PolicyDocument": {
-    "Statement": [
-      {
-      "Action": [
-        "dynamodb:BatchGetItem",
-        "dynamodb:GetRecords",
-        "dynamodb:GetShardIterator",
-        "dynamodb:Query",
-        "dynamodb:GetItem",
-        "dynamodb:Scan",
-        "dynamodb:BatchWriteItem",
-        "dynamodb:PutItem",
-        "dynamodb:UpdateItem",
-        "dynamodb:DeleteItem"
-      ],
-      "Effect": "Allow"  
-    }]
+  expectCDK(stack).to(haveResourceLike("AWS::IAM::Policy", {
+    "PolicyDocument": {
+      "Statement": [
+        {
+        "Action": [
+          "dynamodb:BatchGetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ],
+        "Effect": "Allow"  
+      }]
+    }
   }
-}
-));
+  ));
 });
 
-test('DynamoDB Lambda Created', () => {
-const app = new cdk.App();
-// WHEN
-const stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
-// THEN
-expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
-  "Handler": "index.handler",
-  "Runtime": "nodejs12.x"
-}
-));
+test('Circuit Breaker Lambda Created', () => {
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+    "Handler": "unreliable.handler",
+    "Runtime": "nodejs12.x"
+  }
+  ));
 });
 
 test('API Gateway Http API Created', () => {
-const app = new cdk.App();
-// WHEN
-const stack = new TheLambdaCircuitBreaker.TheLambdaCircuitBreakerStack(app, 'MyTestStack');
-// THEN
-expectCDK(stack).to(haveResourceLike("AWS::ApiGatewayV2::Api", {
-  "ProtocolType": "HTTP"
-}
-));
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGatewayV2::Api", {
+    "ProtocolType": "HTTP"
+  }
+  ));
 });
