236775e44ada10414acb64b2b0e6c6829a1b176c
Matt C <matthew.coulter@liberty-it.co.uk>
2020-11-11 13:39:26 +0000

    tests added for waf

--- the-waf-apigateway/typescript/test/the-waf-apigateway.test.ts
+++ the-waf-apigateway/typescript/test/the-waf-apigateway.test.ts
@@ -1,15 +1,41 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResourceLike, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as WAF from '../lib/the-waf-stack';
 
-test('Empty Stack', () => {
+test('Rules Added To WebACL', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new WAF.TheWafStack(app, 'MyTestStack', {
       gatewayARN: '12345'
     });
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(haveResourceLike("AWS::WAFv2::WebACL", {
+      "Scope": "REGIONAL",
+      "Rules": [
+        {
+          "Name": "AWS-AWSManagedRulesCommonRuleSet"
+        },
+        {
+          "Name": "awsAnonymousIP"
+        },
+        {
+          "Name": "awsIPReputation"
+        },
+        {
+          "Name": "geoblockRule"
+        }
+      ]
+    }))
+});
+
+test('WebACL Associated', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new WAF.TheWafStack(app, 'MyTestStack', {
+    gatewayARN: '12345'
+  });
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::WAFv2::WebACLAssociation", {
+    "ResourceArn": "12345"
+  }))
 });
