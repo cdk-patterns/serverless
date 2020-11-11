d9952ff3d09d1126d1b634ad5bd63e3e49ba87c6
Matt C <matthew.coulter@liberty-it.co.uk>
2020-11-11 13:46:08 +0000

    more specific tests

--- the-waf-apigateway/typescript/test/the-waf-apigateway.test.ts
+++ the-waf-apigateway/typescript/test/the-waf-apigateway.test.ts
@@ -1,6 +1,7 @@
 import { expect as expectCDK, haveResourceLike, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as WAF from '../lib/the-waf-stack';
+import * as Gateway from '../lib/api-gateway-stack';
 
 test('Rules Added To WebACL', () => {
     const app = new cdk.App();
@@ -13,16 +14,40 @@ test('Rules Added To WebACL', () => {
       "Scope": "REGIONAL",
       "Rules": [
         {
-          "Name": "AWS-AWSManagedRulesCommonRuleSet"
+          "Name": "AWS-AWSManagedRulesCommonRuleSet",
+          "Priority": 1,
+          "Statement": {
+            "ManagedRuleGroupStatement": {
+              "Name": "AWSManagedRulesCommonRuleSet"
+            }
+          }
         },
         {
-          "Name": "awsAnonymousIP"
+          "Name": "awsAnonymousIP",
+          "Priority": 2,
+          "Statement": {
+            "ManagedRuleGroupStatement": {
+              "Name": "AWSManagedRulesAnonymousIpList"
+            }
+          }
         },
         {
-          "Name": "awsIPReputation"
+          "Name": "awsIPReputation",
+          "Priority": 3,
+          "Statement": {
+            "ManagedRuleGroupStatement": {
+              "Name": "AWSManagedRulesAmazonIpReputationList"
+            }
+          }
         },
         {
-          "Name": "geoblockRule"
+          "Name": "geoblockRule",
+          "Priority": 4,
+          "Statement": {
+            "GeoMatchStatement": {
+              "CountryCodes": ["NZ"]
+            }
+          }
         }
       ]
     }))
@@ -37,5 +62,25 @@ test('WebACL Associated', () => {
   // THEN
   expectCDK(stack).to(haveResourceLike("AWS::WAFv2::WebACLAssociation", {
     "ResourceArn": "12345"
+  }))
+});
+
+test('API Gateway Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Gateway.ApigatewayStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::RestApi", {
+  }))
+});
+
+test('Lambda Function Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Gateway.ApigatewayStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResourceLike("AWS::Lambda::Function", {
+    "Handler": "helloworld.handler",
+    "Runtime": "nodejs12.x"
   }))
 });
