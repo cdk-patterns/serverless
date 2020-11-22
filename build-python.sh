51f8defbb83645465f13239e8838b8284304362a
Matt Coulter <59790145+cdk-patterns@users.noreply.github.com>
2020-11-22 20:11:48 +0000

    update cw dashboard to use new integrations class for http api

--- build-python.sh
+++ build-python.sh
@@ -6,14 +6,14 @@ do
      python3 -m venv .env;
      source .env/bin/activate;
      pip3 install -r requirements.txt;
-     readarray -t stacks < <(npx -q cdk ls);
+     readarray -t stacks < <(npx -q cdk@1.74.0 ls);
      if [[ "${#stacks[@]}" == 0 ]]; then
        exit 1
      fi
      for stack in "${stacks[@]}"
        do
           echo "npx cdk synth $stack";
-          npx cdk synth "$stack";
+          npx cdk@1.74.0 synth "$stack";
        done    
      deactivate
    fi
--- the-cloudwatch-dashboard/python/the_cloudwatch_dashboard/the_cloudwatch_dashboard_stack.py
+++ the-cloudwatch-dashboard/python/the_cloudwatch_dashboard/the_cloudwatch_dashboard_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigatewayv2 as api_gw,
+    aws_apigatewayv2_integrations as integrations,
     aws_dynamodb as dynamo_db,
     aws_sns as sns,
     aws_cloudwatch as cloud_watch,
@@ -43,7 +44,7 @@ class TheCloudwatchDashboardStack(core.Stack):
 
         # defines an API Gateway Http API resource backed by our "dynamoLambda" function.
         api = api_gw.HttpApi(self, 'HttpAPI',
-                             default_integration=api_gw.LambdaProxyIntegration(handler=dynamo_lambda));
+                             default_integration=integrations.LambdaProxyIntegration(handler=dynamo_lambda));
 
         core.CfnOutput(self, 'HTTP API Url', value=api.url);
 
