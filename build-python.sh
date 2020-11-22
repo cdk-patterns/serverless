32e7f4c67444bcc0f34ecba50f4e3b7d36283d51
Matt Coulter <ubuntu@ip-172-31-11-255.ec2.internal>
2020-11-22 10:04:54 +0000

    update author

---
+++ build-python.sh
@@ -0,0 +1,18 @@
+readarray -t patterns < <(jq -r '.[] | keys | .[]' info.json);
+for pattern in "${patterns[@]}"
+do
+   cd $pattern/python;
+   if test -f "requirements.txt"; then
+     python3 -m venv .env;
+     source .env/bin/activate;
+     pip3 install -r requirements.txt;
+     readarray -t stacks < <(cdk ls);
+     for stack in "${stacks[@]}"
+       do
+          echo "npx cdk synth $stack";
+          npx cdk synth "$stack";
+       done    
+     deactivate
+   fi
+   cd ../../;
+done
