c115d7b87c15a8181678174db465f7b01fc21a21
Matt C <matthew.coulter@liberty-it.co.uk>
2020-12-03 17:00:22 +0000

    python logic

--- the-predictive-lambda/python/the_predictive_lambda/the_predictive_lambda_stack.py
+++ the-predictive-lambda/python/the_predictive_lambda/the_predictive_lambda_stack.py
@@ -1,4 +1,10 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_apigatewayv2 as api_gw,
+    aws_apigatewayv2_integrations as integrations,
+    core
+)
+import os
 
 
 class ThePredictiveLambdaStack(core.Stack):
@@ -6,4 +12,14 @@ class ThePredictiveLambdaStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # defines an AWS  Lambda resource
+        model_folder = os.path.dirname(os.path.realpath(__file__)) + "/../model"
+        predictive_lambda = _lambda.DockerImageFunction(self, 'PredictiveLambda',
+                                                        code=_lambda.DockerImageCode.from_image_asset(model_folder),
+                                                        memory_size=4096,
+                                                        timeout=core.Duration.seconds(15))
+        # defines an API Gateway Http API resource backed by our "PredictiveLambda" function.
+        api = api_gw.HttpApi(self, 'Endpoint',
+                             default_integration=integrations.LambdaProxyIntegration(handler=predictive_lambda));
+
+        core.CfnOutput(self, 'HTTP API Url', value=api.url);
