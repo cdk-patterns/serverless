1c6567a3c67dd45ccc5e61f9cb629812c83edcab
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2020-12-18 13:45:15 +0000

    reformatting

--- the-media-live-stream/typescript/lib/the-media-live-stream-stack.ts
+++ the-media-live-stream/typescript/lib/the-media-live-stream-stack.ts
@@ -14,7 +14,47 @@ const configuration = {
   "hls_stream_order": "ORIGINAL"
 }
 
-const INLINE_POLICY = {"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents","logs:DescribeLogStreams","logs:DescribeLogGroups"],"Resource":"arn:aws:logs:*:*:*"},{"Effect":"Allow","Action":["mediaconnect:ManagedDescribeFlow","mediaconnect:ManagedAddOutput","mediaconnect:ManagedRemoveOutput"],"Resource":"*"},{"Effect":"Allow","Action":["ec2:describeSubnets","ec2:describeNetworkInterfaces","ec2:createNetworkInterface","ec2:createNetworkInterfacePermission","ec2:deleteNetworkInterface","ec2:deleteNetworkInterfacePermission","ec2:describeSecurityGroups"],"Resource":"*"},{"Effect":"Allow","Action":["mediapackage:DescribeChannel"],"Resource":"*"}]}
+const INLINE_POLICY = {
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:DescribeLogGroups"
+    ],
+    "Resource": "arn:aws:logs:*:*:*"
+  },
+  {
+    "Effect": "Allow",
+    "Action": [
+      "mediaconnect:ManagedDescribeFlow",
+      "mediaconnect:ManagedAddOutput",
+      "mediaconnect:ManagedRemoveOutput"
+    ],
+    "Resource": "*"
+  },
+  {
+    "Effect": "Allow",
+    "Action": [
+      "ec2:describeSubnets",
+      "ec2:describeNetworkInterfaces",
+      "ec2:createNetworkInterface",
+      "ec2:createNetworkInterfacePermission",
+      "ec2:deleteNetworkInterface",
+      "ec2:deleteNetworkInterfacePermission",
+      "ec2:describeSecurityGroups"
+    ],
+    "Resource": "*"
+  },
+  {
+    "Effect": "Allow",
+    "Action": ["mediapackage:DescribeChannel"],
+    "Resource": "*"
+  }]
+}
 
 export class TheMediaLiveStreamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -23,16 +63,16 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
     /*
     * First step: Create MediaPackage Channel
     */
-    const channel = new mediapackage.CfnChannel(scope=this, 
-                                                id=`media-package-channel-${configuration["id_channel"]}`, {
-                                                  id: configuration["id_channel"],
-                                                  description: `Channel ${configuration["id_channel"]}`
-                                                });
-    
+    const channel = new mediapackage.CfnChannel(scope = this,
+      id = `media-package-channel-${configuration["id_channel"]}`, {
+      id: configuration["id_channel"],
+      description: `Channel ${configuration["id_channel"]}`
+    });
+
     /*
     * Second step: Add a HLS endpoint to MediaPackage Channel and output the URL of this endpoint
     */
-    const hlsPackage:mediapackage.CfnOriginEndpoint.HlsPackageProperty = {
+    const hlsPackage: mediapackage.CfnOriginEndpoint.HlsPackageProperty = {
       segmentDurationSeconds: configuration["hls_segment_duration_seconds"],
       playlistWindowSeconds: configuration["hls_playlist_window_seconds"],
       streamSelection: {
@@ -42,15 +82,15 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
       }
     }
 
-    const hls_endpoint = new mediapackage.CfnOriginEndpoint(scope=this,
-                                                            id=`endpoint${configuration["id_channel"]}`, {
-                                                              channelId: configuration["id_channel"],
-                                                              id: `endpoint${configuration["id_channel"]}`,
-                                                              hlsPackage
-                                                            });
+    const hls_endpoint = new mediapackage.CfnOriginEndpoint(scope = this,
+      id = `endpoint${configuration["id_channel"]}`, {
+      channelId: configuration["id_channel"],
+      id: `endpoint${configuration["id_channel"]}`,
+      hlsPackage
+    });
 
     // Output the url stream to player
-    new cdk.CfnOutput(scope=this, id="media-package-url-stream", {
+    new cdk.CfnOutput(scope = this, id = "media-package-url-stream", {
       value: hls_endpoint.attrUrl
     });
 
@@ -62,36 +102,36 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
     * Input Security Group
     * Allow 0.0.0.0/0 - Modify it if you want
     */
-    const security_groups_input = new medialive.CfnInputSecurityGroup(scope=this, 
-                                                                    id="media-live-sg-input", {
-                                                                      whitelistRules: [{"cidr":configuration["ip_sg_input"]}]
-                                                                    });
+    const security_groups_input = new medialive.CfnInputSecurityGroup(scope = this,
+      id = "media-live-sg-input", {
+      whitelistRules: [{ "cidr": configuration["ip_sg_input"] }]
+    });
 
     /*
     * Input with destinations output
     */
-    const medialive_input = new medialive.CfnInput(scope=this, 
-                                                    id="meddia-input-channel",{
-                                                      name: `input- ${configuration["id_channel"]}`,
-                                                      type: "RTMP_PUSH",
-                                                      inputSecurityGroups: [security_groups_input.ref],
-                                                      destinations: [{streamName: configuration["stream_name"]}]
-                                                    });     
+    const medialive_input = new medialive.CfnInput(scope = this,
+      id = "meddia-input-channel", {
+      name: `input- ${configuration["id_channel"]}`,
+      type: "RTMP_PUSH",
+      inputSecurityGroups: [security_groups_input.ref],
+      destinations: [{ streamName: configuration["stream_name"] }]
+    });
 
     /*
     * Media Live Channel Block
     */
 
     // IAM Role
-    let iamRole = new iam.Role(scope=this, id="medialive_role", {
-                      roleName: "medialive_role",
-                      assumedBy: new iam.ServicePrincipal('medialive.amazonaws.com'),
-                      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElementalMediaLiveFullAccess')],
-                      inlinePolicies: {"medialivecustom": iam.PolicyDocument.fromJson(INLINE_POLICY)}
-                    });
+    let iamRole = new iam.Role(scope = this, id = "medialive_role", {
+      roleName: "medialive_role",
+      assumedBy: new iam.ServicePrincipal('medialive.amazonaws.com'),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElementalMediaLiveFullAccess')],
+      inlinePolicies: { "medialivecustom": iam.PolicyDocument.fromJson(INLINE_POLICY) }
+    });
 
     // Channel
-    var channelLive = new medialive.CfnChannel(scope=this, id=`media-live-channel-${configuration["id_channel"]}`,{
+    var channelLive = new medialive.CfnChannel(scope = this, id = `media-live-channel-${configuration["id_channel"]}`, {
       channelClass: "SINGLE_PIPELINE",
       name: configuration["id_channel"],
       inputSpecification: {
@@ -99,7 +139,7 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
         maximumBitrate: "MAX_20_MBPS",
         resolution: "HD"
       },
-      inputAttachments:[{
+      inputAttachments: [{
         inputId: medialive_input.ref,
         inputAttachmentName: "attach-input"
       }],
@@ -151,7 +191,7 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
           }
         }],
         // Video descriptions
-        videoDescriptions:[{
+        videoDescriptions: [{
           codecSettings: {
             h264Settings: {
               adaptiveQuantization: "HIGH",
@@ -259,8 +299,8 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
         }]
       },
       roleArn: iamRole.roleArn
-    });   
-    
+    });
+
     // We need to add dependency because CFN must wait channel creation finish before starting the endpoint creation  
     var mediadep = new cdk.ConcreteDependable();
     mediadep.add(channel);
@@ -268,5 +308,5 @@ export class TheMediaLiveStreamStack extends cdk.Stack {
     channelLive.node.addDependency(mediadep);
 
   }
-  
+
 }
