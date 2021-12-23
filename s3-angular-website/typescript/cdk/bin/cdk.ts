e9744df4cdb20a413dc16e46d5beb230c8afe6c4
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2021-12-23 10:44:45 +0000

    update s3-angular pattern to v2

--- s3-angular-website/typescript/cdk/bin/cdk.ts
+++ s3-angular-website/typescript/cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
+import { App } from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
-const app = new cdk.App();
+const app = new App();
 new CdkStack(app, 'S3AngularWebsite');
