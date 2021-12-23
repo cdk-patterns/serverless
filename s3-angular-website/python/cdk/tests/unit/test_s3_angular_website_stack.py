cb6216f8f33a777d073ab87be2a53b8d7c5ff22b
Matt Coulter <1432881+nideveloper@users.noreply.github.com>
2021-12-23 11:28:37 +0000

    update python unit tests

--- s3-angular-website/python/cdk/tests/unit/test_s3_angular_website_stack.py
+++ s3-angular-website/python/cdk/tests/unit/test_s3_angular_website_stack.py
@@ -3,13 +3,31 @@ import aws_cdk.assertions as assertions
 
 from s3_angular_website.s3_angular_website_stack import S3AngularWebsiteStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in s3_angular_website/s3_angular_website_stack.py
-def test_sqs_queue_created():
+
+def test_website_created():
     app = core.App()
     stack = S3AngularWebsiteStack(app, "s3-angular-website")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties('AWS::S3::Bucket',
+                                     assertions.Match.object_like({
+                                         "WebsiteConfiguration": {
+                                             "IndexDocument": 'index.html',
+                                         },
+                                     }))
+
+    template.has_resource('Custom::CDKBucketDeployment', {})
+
+    template.has_resource_properties('AWS::S3::BucketPolicy',
+                                     assertions.Match.object_like({
+                                         "PolicyDocument": {
+                                             "Statement": [
+                                                 assertions.Match.object_like({
+                                                     "Action": 's3:GetObject',
+                                                     "Effect": 'Allow',
+                                                     "Principal": {
+                                                         "AWS": "*"
+                                                     },
+                                                 })],
+                                         },
+                                     }))
